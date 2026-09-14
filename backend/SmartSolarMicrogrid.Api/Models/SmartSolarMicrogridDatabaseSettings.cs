909d4d46aecbea247580a8aa6bdc6ab274d245db
Isara Ekanayake <isaraekanayake123@gmail.com>
2026-09-14 09:55:46 +0530

    feat: add MongoDB connection to API

---
+++ backend/SmartSolarMicrogrid.Api/Models/SmartSolarMicrogridDatabaseSettings.cs
@@ -0,0 +1,7 @@
+namespace SmartSolarMicrogrid.Api.Models;
+
+public class SmartSolarMicrogridDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+}
